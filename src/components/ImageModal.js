adf66f5cd53f428f1ca39d20c4a378f63287bfdf
Nenomancer <nebojsa.kovacevik@gmail.com>
2022-07-03 18:26:46 +0200

    Finish styling image modal

--- src/components/ImageModal.js
+++ src/components/ImageModal.js
@@ -8,19 +8,25 @@ const ImageModal = (props) => {
     props.setShowImageModal(false);
     document.querySelector("body").style.overflowY = "scroll";
   };
+
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") props.setShowImageModal(false);
+  });
+
   return (
     <div className="modal-container">
       <div className="modal-options">
         <button className="btn-inv">Add to album</button>
         <button className="btn">Download</button>
       </div>
-      <MdClose className="close-button" onClick={closeHandler} />
       <img className="modal-image" src={props.selectedImage.src} alt="" />
       <div className="modal-image-info">
         <p className="label">Uploaded by</p>
         <p className="author">{author}</p>
-        <p>width x height</p>
-        <button className="btn-inv">Go back</button>
+        <p className="info">width x height</p>
+        <button className="btn-inv" id="close-modal" onClick={closeHandler}>
+          Go back
+        </button>
       </div>
     </div>
   );
